ae46641fce105071e689648e57cf4782b22a4ec3
LuoBoss <2504944436@qq.com>
2015-04-02 12:31:01 +0800

    补充一个按钮

--- Theme/Team/WorkHard/Index_index.php
+++ Theme/Team/WorkHard/Index_index.php
@@ -2,6 +2,8 @@
     <div class="am-topbar-brand">
         <strong>PESCMS</strong> <small>任务系统</small>
     </div>
+    
+    <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only" data-am-collapse="{target: '#topbar-collapse'}"><span class="am-sr-only">导航切换</span> <span class="am-icon-bars"></span></button>
 
     <div class="am-collapse am-topbar-collapse" id="topbar-collapse">
 
